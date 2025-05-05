69877bbf0f24057d7a02aff387513c80b7b281d4
Jefferson Esperanza <91913021+USER-003@users.noreply.github.com>
2025-05-05 14:08:51 -0600

    feat(part2): phonebook, step 4 - add search input

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { id: 1, name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [newSearch, setNewSearch] = useState("");
 
   const checkNameExistence = () => {
     return persons.some((person) => person.name === newName);
@@ -16,6 +20,7 @@ const App = () => {
 
     if (checkNameExistence()) {
       alert(`${newName} is already added to phonebook`);
+      setNewName("");
       return;
     }
 
@@ -39,9 +44,21 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleNewSearch = (event) => {
+    setNewSearch(event.target.value);
+  };
+
+  const numberToShow = newSearch
+    ? persons.filter((person) =>
+        person.name.toLowerCase().includes(newSearch.toLowerCase())
+      )
+    : persons;
+
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with: <input value={newSearch} onChange={handleNewSearch} />
+      <h2>add a new</h2>
       <form onSubmit={handleAddPerson}>
         <div>
           name: <input value={newName} onChange={handleNewName} required />
@@ -56,7 +73,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
+        {numberToShow.map((person) => (
           <li key={person.id}>
             {person.name} {person.number}
           </li>
