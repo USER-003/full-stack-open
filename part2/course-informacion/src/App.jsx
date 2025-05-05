e604634998cddd01c21222221a6f52dbe01093bb
Jefferson Esperanza <91913021+USER-003@users.noreply.github.com>
2025-05-04 18:23:03 -0600

    feat(part2): add course informacion, step 6

---
+++ part2/course-informacion/src/App.jsx
@@ -0,0 +1,76 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.name} {props.exercises}
+      </p>
+    </>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+    </>
+  );
+};
+
+const Total = ({ parts }) => {
+  return (
+    <>
+      <p>
+        Number of exercises{" "}
+        {parts.reduce((sum, part) => sum + part.exercises, 0)}
+      </p>
+    </>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
